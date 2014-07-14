4cea43e5c3929a1cd36978bfeb1f58082d441463
Marco Minetti <marco.minetti@novetica.org>
2014-07-14 12:16:21 +0200

    Added early fake kernel-related modules (just as code placeholders).

---
+++ core/kernel/preprocess.server.js
@@ -0,0 +1,33 @@
+'use strict';
+/*
+
+ ljve.io - Live Javascript Virtualized Environment
+ Copyright (C) 2010-2014  Marco Minetti <marco.minetti@novetica.org>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+//N: Provides kernel functions to execute and manage environment.
+platform.kernel = platform.kernel || {};
+
+//F: Augments Javascript code for injection to current environment.
+//A: code: Specifies Javascript code to be augmented.
+//A: [file]: Specifies the file name containing code to be augmented.
+//A: [module]: Specifies name of the module that is augmenting code
+//R: Returns the augmented code.
+platform.kernel.preprocess = function(code, module, file){
+  //T: migrate preprocessor stack from 0.3.x branch
+  return code;
+};
