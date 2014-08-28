d9bcab3e1d4b95c1dfdb825a4afcc9c71d652fc3
Marco Minetti <marco.minetti@novetica.org>
2014-08-28 15:08:56 +0200

    Ported file and javascript invoke engine processing from 0.3.x branch.

--- core/engine/engine.server.js
+++ core/engine/engine.server.js
@@ -22,17 +22,52 @@
 //N: Provides server-side specific classes and namespaces.
 platform.engine = platform.engine || {};
 
+if(platform.engine.id == null) {
+  platform.engine.id = native.uuid.v1();
+
+  Object.defineProperty(platform.engine, "id", {
+    configurable: false,
+    enumerable: true,
+    writable: false
+  });
+}
+
+platform.engine.send = {};
+
+platform.engine.send.cache = function(path,tag){
+  if (platform.io.cache.is(path,tag) === true) {
+    var request = context.request;
+    var response = context.response;
+    var server = context.server;
+
+    //T: get last-modified/content-length for header
+
+    var content_type = platform.configuration.server.http.default.mimetypes[native.path.extname(path)] || 'text/plain';
+    response.setHeader("Content-Type", content_type);
+    response._async = true;
+
+    var data_stream = null;
+    if (server.compression.enable === true && request.headers ['accept-encoding'] != null && request.headers ['accept-encoding'].contains ('gzip') === true) {
+      response.setHeader('Content-Encoding', 'gzip');
+      data_stream = platform.io.cache.get.stream(path, tag, false);
+    } else {
+      data_stream = platform.io.cache.get.stream(path, tag, true);
+    }
+    if (request.method === 'HEAD') {
+      response.end();
+    } else {
+      data_stream.pipe(response);
+    }
+  } else {
+    //T: throw error?
+  }
+};
+
 //O: Provides process implementations.
 platform.engine.process = {};
 
+//O: Provides auth implementations.
 platform.engine.process.auth = {};
-
-platform.engine.process.auth.url = function(url){
-  if (platform.configuration.server.http.default.reject.url.test(url) === true) {
-    return false;
-  }
-  return true;
-};
 
 platform.engine.process.auth.basic = function(realm,username,password,callback){
   callback(password === 'password');
@@ -46,48 +81,206 @@ platform.engine.process.auth.digest = function(realm,username,callback){
 };
 
 //F: Processes an HTTP request.
-platform.engine.process.http = function (request, response, server) {
-  var secure = server.secure;
-  var debug = server.debug;
-  var port = server.port;
-  var count = request.id;
-  var remoteAddress = request.client.address;
-  var remotePort = request.client.port;
+platform.engine.process.http = function (context) {
+  var request = context.request;
+  var response = context.response;
+  var call = context.call;
+  var server = context.server;
 
-  //T: handler
-
-  //T: is session valid
+  //T: check session is valid and get session object
   var session = null;
+  if ((request.headers['x-platform-session'] != null && request.headers['x-platform-token'] != null) || (call.arguments['auth_session'] != null && call.arguments['auth_token'])) {
+    var session_id = request.headers['x-platform-session'] || call.arguments['auth_session'];
+    var session_token = request.headers['x-platform-token'] || call.arguments['auth_token'];
+    if (platform.session.exist(session_id) === true) {
+      session = platform.session.get(session_id);
+      if (session != null && session.identity.token === session_token) {
+        if (session._session.timeout > 0) {
+          if (session.state === 0) {
+            session._session.timeout = platform.configuration.engine.session.gc.state.http;
+            session.state = 1;
+            platform.statistics.counter('sessions.gen1').inc();
+            platform.statistics.counter('sessions.gen0').dec();
+            if(platform.configuration.server.debugging.session === true) {
+              console.debug('session %s moved to state 1 (http alive)', session.name);
+            }
+            //T: reauthenticate request from server?
+          }
+          session._session.lease = Date.now() + session._session.timeout;
+        }
+      } else {
+        //T: log intrusion attempt?
+        session = null;
+      }
+    }
+  }
+  if (session == null && context.request.headers ["x-platform-bootstrap"] != null) {
+    var bootstrap_id = request.headers['x-platform-bootstrap'] || call.arguments['auth_bootstrap'];
+    if (platform._bootstrap.isValid(bootstrap_id) === true) {
+      if (platform._bootstrap.exist(bootstrap_id) === true) {
+        session = platform._bootstrap.get(bootstrap_id);
+        if (session != null){
+          session._session.lease = Date.now() + session._session.timeout;
+        }
+      }
+    }
+  }
+  context.session = session;
+
+  if (session != null) {
+    session._session.working++;
+    request.on('end',function(){
+      session._session.working--;
+    });
+  }
 
   //T: get identity
   var identity = null;
+  if (session != null) {
+    //T: migrate classes and integrity checks from 0.3.x branch
+    identity = session.identity._store;
+  }
+  context.identity = identity;
 
-  platform.server.http.context.create(request, response, server, session, identity, function(err,context){
-    native.domain.active.__context__ = context;
-    platform.engine.process.call();
-  });
+  //C: handling url path
+  switch(call.url.pathname){
+    case '/':
+      platform.kernel.get(platform.configuration.client.bootstrap.root)();
+      return;
+    case '/-':
+      platform.kernel.get(platform.configuration.client.bootstrap.loader)();
+      return;
+  }
 
-  //T: check if exists
+  try {
+    var target = platform.kernel.get(call.url.pathname, null, '/');
+    var skip = false;
 
-  //T: check if protected
+    //T: check if protected/unprotected
 
-  //T: check if restful
+    if (skip === false) {
+      platform.engine.process.restful(target);
+    } else {
+      platform.engine.process.fallback();
+    }
+  } catch(err) {
+    platform.engine.process.fallback();
+  }
+
+};
+
+platform.engine.process.fallback = function(){
+  //T: check if handler exists
+  //T: process handler
 
   //T: process file
-
-
+  platform.engine.process.file();
 };
 
-platform.engine.process.call = function(){
-  context.response.end();
+platform.engine.process.restful = function(target){
+  var request = context.request;
+  var response = context.response;
+  var call = context.call;
+
+  response.setHeader('Cache-Control','no-cache, no-store, must-revalidate');
+  response.setHeader('Pragma','no-cache');
+  response.setHeader('Expires','0');
+
+  try {
+    switch (typeof target) {
+      case 'function':
+        var async_callback = false;
+        var invoke_arguments = [];
+        Function.info.arguments(target).forEach(function (argument) {
+          if (argument === 'callback') {
+            async_callback = true;
+            response._async = true;
+            invoke_arguments.push(function (err, result) {
+              platform.engine.process.restful._callback(err,result);
+            });
+          } else {
+            invoke_arguments.push(call.arguments[argument]);
+          }
+        });
+        if (async_callback === false){
+          var result = target.apply(target, invoke_arguments);
+          if (result != null && (result.constructor === Error || result.constructor === Exception)) {
+            platform.engine.process.restful._callback(result);
+          } else{
+            platform.engine.process.restful._callback(null, result);
+          }
+        } else {
+          target.apply(target, invoke_arguments);
+        }
+        break;
+    }
+  } catch(err) {
+    platform.engine.process.restful._callback(err);
+  }
 };
 
-platform.engine.process.restful = function(){
-  context.response.end();
+platform.engine.process.restful._callback = function(err,result){
+  var request = context.request;
+  var response = context.response;
+  var server = context.server;
+
+  response.setHeader('Content-Type','application/json');
+
+  var content = null;
+  if (err == null) {
+    content = JSON.stringify({
+      'result': result
+    });
+  } else {
+    content = JSON.stringify({
+      'error': err
+    });
+  }
+  var content_length = content.length;
+
+  var data_stream = response;
+  if (server.compression.enable === true && content_length > server.compression.limit && request.headers ['accept-encoding'] != null && request.headers ['accept-encoding'].contains ('gzip') === true) {
+    response.setHeader ('Content-Encoding', 'gzip');
+    data_stream = native.zlib.createGzip();
+    data_stream.pipe(response);
+  }
+
+  if (request.method !== 'HEAD') {
+    data_stream.write(content);
+  }
+  data_stream.end();
+};
+
+platform.engine.process.handler = function(){
+
 };
 
 platform.engine.process.file = function(){
-  context.response.end();
+  var request = context.request;
+  var response = context.response;
+  var server = context.server;
+  var path = context.call.url.pathname;
+  if (platform.io.exist(path) === true) {
+    var file_info = platform.io.info(path);
+    var last_modified = file_info.atime;
+    var content_type = platform.configuration.server.http.default.mimetypes[native.path.extname(path)] || 'text/plain';
+    var content_length = file_info.size;
+    response.setHeader("Last-Modified", last_modified.toUTCString());
+    response.setHeader("Content-Type", content_type);
+    response.setHeader("Content-Length", content_length);
+    response._async = true;
+    var data_stream = platform.io.get.stream(path);
+    if (server.compression.enable === true && content_length > server.compression.limit && request.headers ['accept-encoding'] != null && request.headers ['accept-encoding'].contains ('gzip') === true) {
+      response.setHeader ('Content-Encoding', 'gzip');
+      data_stream = data_stream.pipe(native.zlib.createGzip());
+    }
+    data_stream.pipe(context.response);
+  } else {
+    //C: returning 404 status code (for security reason we do not reply with 403/406)
+    response.statusCode = 404;
+    //T: return json, html or text depending on acceptable content type
+    response.end ("404 File not found");
+  }
 };
 
 //F: Processes a new WebSocket.
