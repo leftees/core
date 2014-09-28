99c1eb843dbf0c6a5983674c655a3880d527fc66
Marco Minetti <marco.minetti@novetica.org>
2014-09-28 12:53:19 +0200

    Removed 'use strict'. Updated code documentation.

--- core/environment/load.server.js
+++ core/environment/load.server.js
@@ -1,8 +1,7 @@
-'use strict';
 /*
 
  ljve.io - Live Javascript Virtualized Environment
- Copyright (C) 2010-2014  Marco Minetti <marco.minetti@novetica.org>
+ Copyright (C) 2010-2014 Marco Minetti <marco.minetti@novetica.org>
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Affero General Public License as published by
@@ -19,11 +18,19 @@
 
  */
 
+//N: Provides environment management classes and methods.
 platform.environment = platform.environment || {};
 
+//O: Stores the files loaded inside the execution environment (both  server/client side)
+//H: Files are pushed with relative path as passed to platform.environment.load.
 platform.environment._files = platform.environment._files || [];
 
+//F: Loads a file into the execution environment.
+//A: path: Specifies the path of the file to be loaded.
+//A: [module]: Specifies the module name requiring the file.
+//H: The file type is automatically detect by extension for proper loading.
 platform.environment.load = function(path,module){
+  //T: support modular/extensible file type support
   if (platform.environment._files.indexOf(path) === -1) {
     platform.environment._files.push(path);
   }
