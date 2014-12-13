4f70cb8223e7bb7a130055795ffe5db5ea4e5cce
marco.minetti <marco.minetti@novetica.org>
2014-12-13 14:57:20 +0100

    Removed codetags from generated code.

--- core/kernel/preprocess.server.js
+++ core/kernel/preprocess.server.js
@@ -77,7 +77,7 @@ platform.kernel.preprocess = function(code, path, module){
     });
   });
 
-  var generated_object = platform.parser.js.stringify(ast,!platform.runtime.debugging,true);
+  var generated_object = platform.parser.js.stringify(ast,!platform.runtime.debugging,false);
   ast = null;
 
   var generated_code = generated_object.code;
