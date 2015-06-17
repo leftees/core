783107be62910f8076d5953c7cf961c1a8c360f5
marco.minetti <marco.minetti@novetica.org>
2015-06-17 13:31:21 +0200

    Reworked platform.classes namespace implementation.

--- core/kernel/classes.server.js
+++ core/kernel/classes.server.js
@@ -18,19 +18,27 @@
 
  */
 
-//N: Provides class helper to register/unregister and instance environment-level classes.
+/**
+ * Provides class helper to register/unregister and instance environment-level classes.
+ * @namespace
+*/
 platform.classes = platform.classes || {};
 
-//O: Stores the registered classes constructors.
+/**
+ * Stores the registered classes constructors.
+ * @type {Object}
+*/
 platform.classes._store = platform.classes._store || {};
 
-//F: Registers a class into current environment.
-//A: name: Specifies name of new class to register.
-//A: constructor: Specifies constructor with prototype object for new class instances.
-//A: [replace]: Specifies whether existing constructor should be replaced instead of throwing exception. Default is false.
-//R: Returns true if class is successfully registered.
-platform.classes.register = function(name,constructor,replace){
-  if (platform.classes.exists(name) === false || replace === true) {
+/**
+ * Registers a class into current environment.
+ * @param {} name Specifies name of new class to register.
+ * @param {} constructor Specifies constructor with prototype object for new class instances.
+ * @param {} [force] Specifies whether to overwrite pre-existent events. Default is false.
+ * @return {} Returns true if class is successfully registered.
+*/
+platform.classes.register = function(name,constructor,force){
+  if (platform.classes.exists(name) === false || force === true) {
     platform.classes._store[name] = constructor;
     return true;
   } else {
@@ -38,9 +46,11 @@ platform.classes.register = function(name,constructor,replace){
   }
 };
 
-//F: Unregisters a class from current environment.
-//A: name: Specifies name of class to unregister.
-//R: Returns true if class is successfully unregistered.
+/**
+ * Unregisters a class from current environment.
+ * @param {} name Specifies name of class to unregister.
+ * @return {} Returns true if class is successfully unregistered.
+*/
 platform.classes.unregister = function(name){
   if (platform.classes.exists(name) === true) {
     return delete platform.classes._store[name];
@@ -49,9 +59,11 @@ platform.classes.unregister = function(name){
   }
 };
 
-//F: Gets a class constructor from current environment.
-//A: name: Specifies name of class to get.
-//R: Returns class constructor.
+/**
+ * Gets a class constructor from current environment.
+ * @param {} name Specifies name of class to get.
+ * @return {} Returns class constructor.
+*/
 platform.classes.get = function(name){
   if (platform.classes.exists(name) === true) {
     return platform.classes._store[name];
@@ -64,17 +76,72 @@ platform.classes.list = function(){
   return Object.keys(platform.classes._store);
 };
 
-//F: Checks whether a class is registered in current environment.
-//A: name: Specifies name of class to check.
-//R: Returns true if class is registered.
+/**
+ * Checks whether a class is registered in current environment.
+ * @param {} name Specifies name of class to check.
+ * @return {} Returns true if class is registered.
+*/
 platform.classes.exists = function(name){
   return (platform.classes._store.hasOwnProperty(name) && typeof platform.classes._store[name] === 'function');
 };
 
-//F: Checks whether an object is an instance of a registered class in current environment.
-//A: object: Specifies object to check.
-//A: name: Specifies name of class to compare.
-//R: Returns true if object is an instance of specified class.
+/**
+ * Checks whether an object is an instance of a registered class in current environment.
+ * @param {} object Specifies object to check.
+ * @param {} name Specifies name of class to compare.
+ * @return {} Returns true if object is an instance of specified class.
+*/
 platform.classes.instanceOf = function(object,name){
   return (object != null && platform.classes._store.hasOwnProperty(name) === true && platform.classes._store[name] === object.constructor);
+};
+
+/**
+ * Gets a class prototype from current environment.
+ * @param {} name Specifies name of class to get.
+ * @return {} Returns class prototype.
+*/
+platform.classes.getPrototype = function(name){
+  if (platform.classes.exists(name) === true) {
+    return platform.classes._store[name].prototype;
+  } else {
+    throw new Exception('constructor for %s not found',name);
+  }
+};
+
+/**
+ * Create a new instance from classes defined in current environment.
+ * @function platform.kernel.new
+ * @param {} name Specifies name of class to instance.
+ * @param {} [args] Specifies the arguments for the instance constructor.
+ * @param {} [root] Specifies which object should be use as root to get property value. Default is global.
+ * @param {} [divisor] Specifies which char/string should be used split the name property tree. Default is '.'.
+ * @return {} Returns the new instance of required class.
+ * @alert  While traversing the property tree every get() property function will be evaluated if any.
+ * @alert  If no custom root is specified, this function will try to instance registered classes through platform.classes.
+*/
+platform.kernel.new = function (name,args,root,divisor) {
+  var target = null;
+  var new_instance = null;
+
+  // looking for registered class if no root has been specified
+  if (root == null) {
+    if (platform.classes.exists(name) === true) {
+      target = platform.classes.get(name);
+    }
+  }
+
+  // trying to resolve strong-name if root has been specified or class is not registered
+  if (target == null){
+    try{
+      var found = platform.kernel._find(name,root,divisor,false,false);
+      target = found.target;
+    } catch(err){
+      throw new Exception(err, 'unable to instance class %s',name);
+    }
+  }
+
+  // creating new class instance
+  new_instance = Object.create(target.prototype);
+  target.apply(new_instance, args);
+  return new_instance;
 };
