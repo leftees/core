731c3310f49aaacbb59cc4c48528f2cdf9aa256a
marco.minetti <marco.minetti@novetica.org>
2015-06-17 13:36:52 +0200

    Removed obsolete test automation implementation.
    This will be re-implemented in further branches.

--- test/files/php/info.php
+++
@@ -1,5 +0,0 @@
-<?php
-	date_default_timezone_set('Europe/Rome');
-	session_start();
-	phpinfo();
-?>
--- test/files/php/service.php
+++
@@ -1,8 +0,0 @@
-<?php
-	date_default_timezone_set('Europe/Rome');
-	
-	$now = date("Y/m/d h:i:s") ;
-	$hello = "Hello from service.php at ";
-
-	echo $hello.$now;
-?>
