0e58fa1336eee9e9c15df18502684bf134bdc00c
marco.minetti <marco.minetti@novetica.org>
2015-06-17 14:31:33 +0200

    Added default template for create main command.

---
+++ core/skel/default/app/main.server.js
@@ -0,0 +1,10 @@
+/*
+ insert here your old-fashioned node.js code to be executed at startup
+ or safely delete this file if you're going to use only ljve modules
+ */
+
+console.warn('insert in /app/main.server.js your old-fashioned node.js code to be executed at startup' +
+  ' or safely delete it if you\'re going to use only ljve modules');
+
+// this module exports are available as "global.app.main" object
+exports.ready = true;
