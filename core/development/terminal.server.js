85dc6eeb29e78edb1869f95e7d9e7471b47e4d56
marco.minetti <marco.minetti@novetica.org>
2015-09-24 15:07:21 +0200

    Implemented terminal develoment tool APIs.
    Missing platform.development.tools.terminal.stop() implementation when embedding is not spawned but in-process.

--- core/development/terminal.server.js
+++ core/development/terminal.server.js
@@ -18,59 +18,194 @@
 
  */
 
-var tty = require('ljve-terminal');
+/**
+ * Provides development classes and objects.
+ * @namespace
+*/
+platform.development = platform.development || {};
 
-var app = tty.createServer({
-  "users": {
-  },
-  /*"https": {
-    "key": "./server.key",
-    "cert": "./server.crt"
-  },*/
-  "port": 9093,
-  "hostname": "0.0.0.0",
-  "shell": "bash",
-  //"shellArgs": ["arg1", "arg2"],
-  "limitGlobal": 1000,
-  "limitPerUser": 100,
-  "localOnly": false,
-  "cwd": process.cwd(),
-  "syncSession": true,
-  "sessionTimeout": 300000,
-  "log": false,
-  "io": { "log": true },
-  "debug": false,
-  "term": {
-    "termName": "xterm",
-    "geometry": [80, 24],
-    "scrollback": 0,
-    "visualBell": true,
-    "popOnBell": true,
-    "cursorBlink": true,
-    "screenKeys": true,
-    "colors": [
-      "#2e3436",
-      "#cc0000",
-      "#4e9a06",
-      "#c4a000",
-      "#3465a4",
-      "#75507b",
-      "#06989a",
-      "#d3d7cf",
-      "#555753",
-      "#ef2929",
-      "#8ae234",
-      "#fce94f",
-      "#729fcf",
-      "#ad7fa8",
-      "#34e2e2",
-      "#eeeeec"
-    ]
+/**
+ * Provides development tools classes and objects.
+ * @namespace
+ */
+platform.development.tools = platform.development.tools || {};
+
+//? if(CLUSTER) {
+if (platform.cluster.worker.master === true) {
+  //? }
+
+  /**
+  * Provides support for terminal.
+  * @namespace
+  */
+  platform.development.tools.terminal = platform.development.tools.terminal || {};
+
+  /**
+   *  Contains default options for tty.js.
+   */
+  platform.development.tools.terminal._options = {
+    "users": {
+    },
+    "port": null,
+    "hostname": "0.0.0.0",
+    "shell": "bash",
+    "limitGlobal": 1000,
+    "limitPerUser": 100,
+    "localOnly": false,
+    "cwd": process.cwd(),
+    "syncSession": true,
+    "sessionTimeout": 300000,
+    "log": false,
+    "io": { "log": false },
+    "debug": false,
+    "term": {
+      "termName": "xterm",
+      "geometry": [80, 24],
+      "scrollback": 0,
+      "visualBell": true,
+      "popOnBell": true,
+      "cursorBlink": true,
+      "screenKeys": true,
+      "colors": [
+        "#2e3436",
+        "#cc0000",
+        "#4e9a06",
+        "#c4a000",
+        "#3465a4",
+        "#75507b",
+        "#06989a",
+        "#d3d7cf",
+        "#555753",
+        "#ef2929",
+        "#8ae234",
+        "#fce94f",
+        "#729fcf",
+        "#ad7fa8",
+        "#34e2e2",
+        "#eeeeec"
+      ]
+    }
+  };
+
+  /**
+   *  Checks whether the tool is running.
+  */
+  platform.development.tools.terminal._running = platform.development.tools.terminal._running || false;
+
+  /**
+   *  Contains forked child process object when running.
+  */
+  platform.development.tools.terminal._wrapper = platform.development.tools.terminal._wrapper || undefined;
+
+  /**
+   *  Contains forked child process object when running.
+   */
+  platform.development.tools.terminal._process = platform.development.tools.terminal._process || undefined;
+
+  /**
+   *  Define separated process path for terminal.
+   */
+  platform.development.tools.terminal._process_path = '/node_modules/ljve-terminal/bin/ljve-terminal';
+
+  /**
+  * Starts the terminal tool.
+  * @param {} [port] Specifies the port argument to bind for the web UI.
+  * @return {} None.
+ * @alert  Throws exception if tool is already running.
+  */
+  platform.development.tools.terminal.start = function (port) {
+    if (platform.development.tools.terminal._running === false) {
+      platform.development.tools.terminal._options.port = port;
+      if (platform.configuration.development.tools.terminal.spawn === false) {
+        if (platform.development.tools.terminal._wrapper === undefined) {
+          platform.development.tools.terminal._wrapper = require('ljve-terminal').createServer(platform.development.tools.terminal._options);
+        }
+        platform.development.tools.terminal._wrapper.listen();
+      } else {
+        // saving options configuration file
+        var data_fs = platform.io.store.getByName('root');
+        if (data_fs.exists('/data/terminal/') === false){
+          data_fs.create('/data/terminal/');
+        }
+        data_fs.set.stringSync('/data/terminal/config.json',JSON.stringify(platform.development.tools.terminal._options));
+        // executing tool separate process(es)
+        platform.development.tools.terminal._process = require('child_process').spawn(process.execPath, [
+          platform.configuration.runtime.path.core + platform.development.tools.terminal._process_path,
+          '--config',
+          native.path.join(data_fs.base,'/data/terminal/config.json')
+        ],{stdio:'inherit'});
+      }
+      platform.development.tools.terminal._running = true;
+    } else {
+      throw new Exception('terminal tool is already running');
+    }
+  };
+
+  /**
+  * Stops the terminal tool.
+  * @return {} None.
+ * @alert  Throws exception if tool is not running.
+  */
+  platform.development.tools.terminal.stop = function () {
+    if (platform.development.tools.terminal._running === true) {
+      if (platform.configuration.development.tools.terminal.spawn === false) {
+        //TODO: check/implement stop in ljve-terminal
+        //platform.development.tools.terminal._wrapper.close();
+      } else {
+        // killing separate process(es)
+        platform.development.tools.terminal._process.kill();
+        platform.development.tools.terminal._process = undefined;
+      }
+      platform.development.tools.terminal._running = false;
+    } else {
+      throw new Exception('terminal tool is not running');
+    }
+  };
+
+  if (platform.state === platform._states.PRELOAD) {
+
+    // defining running property
+    Object.defineProperty(platform.development.tools.terminal, 'running', {
+      get: function () {
+        return platform.development.tools.terminal._running;
+      }, set: function () {
+      }
+    });
+
+    // attaching exit events to kill tool
+    ['exit'].forEach(function (e) {
+      process.on(e, function () {
+        try {
+          // stopping terminal before exit
+          if (platform.development.tools.terminal._running === true) {
+            platform.development.tools.terminal.stop();
+          }
+        } catch (err) {
+        }
+      });
+    });
+
+    if (platform.configuration.runtime.development === true) {
+    platform.events.attach('core.ready','devtools.init.terminal',function(){
+      // starting development tools (only if development is enabled - NODE_ENV=development)
+      platform.development.tools.terminal.start(platform.configuration.development.tools.terminal.ports.ui);
+    });
+
+    platform.events.attach('core.ready','devtools.init.terminal.ui', function(){
+      console.warn('terminal ui available at http://localhost:%s/', platform.configuration.development.tools.terminal.ports.ui);
+    });
   }
-});
 
-app.setAuth(function(a,b,c){
- c();
-});
+  }
+  //? if(CLUSTER) {
+}
+//? }
 
-app.listen();
+
+
+
+
+
+//app.setAuth(function(a,b,c){
+//c();
+//});
