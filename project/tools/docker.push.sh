9f212327cc104f18d605812646a24db7a2c3d648
marco.minetti <marco.minetti@novetica.org>
2015-10-01 18:33:57 +0200

    Fixed docker push project tool.

--- project/tools/docker.push.sh
+++ project/tools/docker.push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-ubuntu=`docker run -i -t --entrypoint="bash"  ljveio/core:latest -c "lsb_release -sr"`
+ubuntu=`docker run -t --entrypoint="bash"  ljveio/core:latest -c "lsb_release -sr"`
 ubuntu=${ubuntu::-1}
-node=`docker run -i -t --entrypoint="bash"  ljveio/core:latest -c "node -v"`
+node=`docker run -t --entrypoint="bash"  ljveio/core:latest -c "node -v"`
 node=${node:1:-1}
-ljve=`docker run -i -t --entrypoint="bash"  ljveio/core:latest -c "npm -g view ljve.io version"`
+ljve=`docker run -t --entrypoint="bash"  ljveio/core:latest -c "npm -g view ljve.io version"`
 ljve=${ljve::-1}
 tag=$ubuntu-$node-$ljve
 echo $tag
-docker tag ljveio/core:latest ljveio/core:$tag
+docker tag -f ljveio/core:latest ljveio/core:$tag
 docker push ljveio/core:latest
 docker push ljveio/core:$tag
